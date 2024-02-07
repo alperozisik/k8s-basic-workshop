4ab489516b7d4cbcb8e2477a1822c7172a5259b6
Alper Özışık <alper.ozisik@oracle.com>
2024-02-08 00:50:09 +0300

    changes

---
+++ scripts/cri-o.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+VERSION=1.28
+SUBVERSION=$(echo $VERSION | awk -F'.' '{print $1"."$2}')
+OS=xUbuntu_22.04
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:
+stable.list
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:
+/stable/$OS/ /
+EOF
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:
+stable:cri-o:$VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:
+/stable:/cri-o:/$VERSION/$OS/ /
+EOF
+
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable: cri-o:/$VERSION/$OS/Release.key | sudo apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install cri-o cri-o-runc cri-tools -y
+
+sudo systemctl daemon-reload
+sudo systemctl enable crio --now
---
+++ todo.md
@@ -0,0 +1,3 @@
+- Oracle Linux Support
+- Include Public IP during `kubeadm init` via `apiserver-cert-extra-sans=<InternalIP>,<ExternalIP>` format
+    Use curl to query compute node
