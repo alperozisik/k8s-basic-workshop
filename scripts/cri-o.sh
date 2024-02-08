b13b2f9d4505d5c4009ed45f509ed863923c534e
Alper Özışık <alper.ozisik@oracle.com>
2024-02-08 05:45:23 +0300

    changes

--- scripts/cri-o.sh
+++ scripts/cri-o.sh
@@ -4,20 +4,12 @@ VERSION=1.28
 SUBVERSION=$(echo $VERSION | awk -F'.' '{print $1"."$2}')
 OS=xUbuntu_22.04
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:
-stable.list
-deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:
-/stable/$OS/ /
-EOF
+echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list >/dev/null
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:
-stable:cri-o:$VERSION.list
-deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:
-/stable:/cri-o:/$VERSION/$OS/ /
-EOF
-
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable: cri-o:/$VERSION/$OS/Release.key | sudo apt-key add -
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
+echo "deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list >/dev/null
+sudo mkdir -p /usr/share/keyrings
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
 
 sudo apt-get update
 sudo apt-get install cri-o cri-o-runc cri-tools -y
