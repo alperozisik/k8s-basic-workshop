5687bb757102a722c109c0e022d1299f711f08fe
Alper Özışık <alper.ozisik@oracle.com>
2024-04-28 01:46:25 +0300

    new cri-o install script

--- scripts/cri-o.sh
+++ scripts/cri-o.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=1.28
-SUBVERSION=$(echo $VERSION | awk -F'.' '{print $1"."$2}')
-OS=xUbuntu_22.04
-
-echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list >/dev/null
-
-echo "deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list >/dev/null
-sudo mkdir -p /usr/share/keyrings
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
-
-sudo apt-get update
-sudo apt-get install cri-o cri-o-runc cri-tools -y
+sudo apt install jq -y
+curl https://raw.githubusercontent.com/cri-o/packaging/main/get | sudo bash
 
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
