bce4289778ab782be769f3f6024f71d1fb07f396
Alper Özışık <alper.ozisik@oracle.com>
2024-04-28 15:13:52 +0300

    updated cri-dockerd install

--- scripts/cri-dockerd-install.sh
+++ scripts/cri-dockerd-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.9/cri-dockerd_0.3.9.3-0.ubuntu-jammy_amd64.deb
-sudo dpkg -i ./cri-dockerd_0.3.9.3-0.ubuntu-jammy_amd64.deb
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.13/cri-dockerd_0.3.13.3-0.ubuntu-jammy_amd64.deb
+sudo dpkg -i ./cri-dockerd_0.3.13.3-0.ubuntu-jammy_amd64.deb
 sudo apt-get install -f
-rm ./cri-dockerd_0.3.9.3-0.ubuntu-jammy_amd64.deb
+rm ./cri-dockerd_0.3.13.3-0.ubuntu-jammy_amd64.deb
